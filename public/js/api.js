79df3d7911e41b98230291a99bb75a7777b2346d
João Batista Rodrigues Ribeiro <87319898+Jbrr2021@users.noreply.github.com>
2022-12-09 20:53:51 -0300

    Netflix-clone
    Construindo um Clone Responsivo do NetFlix com CSS, HTML, JavaScript e NodeJS.
    Aula do Felipe Aguiar - DIO.

---
+++ public/js/api.js
@@ -0,0 +1,7 @@
+let api_key = "930aa24e726a65db155d877e7ddbcbd5"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?"
